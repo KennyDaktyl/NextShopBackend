839533a62026f39c972225bd131be8ef459abc60
Michał Pielak <michal.pielak81@gmail.com>
2024-08-06 13:05:05 +0200

    Dodanie opcji dla produktu

--- web/carts/views.py
+++ web/carts/views.py
@@ -1,4 +1,3 @@
-# views.py
 import uuid
 
 from django.http import JsonResponse
@@ -12,118 +11,161 @@ from web.carts.cart import Cart
 from web.models.products import Product
 from web.models.products import ProductVariant as Variant
 
-from .serializers import CartItemSerializer
+from .serializers import (CartCreateSerializer, CartItemSerializer,
+                          CartUpdateSerializer, RemoveItemSerializer,
+                          UpdateCartItemQtySerializer)
 
 
 class CartCreateView(GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = CartCreateSerializer
 
     def post(self, request, *args, **kwargs):
-        body = request.data
-        product_id = body.get("product_id")
-        quantity = int(body.get("quantity", 1))
-        variant_id = body.get("variant_id")
-
-        product = get_object_or_404(Product, id=product_id)
-        variant = None
-        if variant_id:
-            variant = get_object_or_404(Variant, id=variant_id)
-            available_quantity = variant.qty
-        else:
-            available_quantity = product.qty
-
-        if quantity > available_quantity:
-            return Response(
-                {"error": "Requested quantity exceeds available stock"},
-                status=status.HTTP_400_BAD_REQUEST,
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            product_id = serializer.validated_data.get("product_id")
+            quantity = serializer.validated_data.get("quantity")
+            variant_id = serializer.validated_data.get("variant_id")
+            selected_option = serializer.validated_data.get(
+                "selected_option", None
             )
 
-        image = product.item_image if variant is None else variant.item_image
+            product = get_object_or_404(Product, id=product_id)
+            variant = None
+            if variant_id:
+                variant = get_object_or_404(Variant, id=variant_id)
+                available_quantity = variant.qty
+            else:
+                available_quantity = product.qty
 
-        cart_item_data = {
-            "id": product_id,
-            "name": product.name,
-            "slug": product.slug,
-            "price": str(product.current_price),
-            "variant": variant.name if variant else None,
-            "quantity": quantity,
-            "available_quantity": available_quantity,
-            "image": image,
-            "url": product.get_absolute_url(),
-        }
+            if quantity > available_quantity:
+                return Response(
+                    {"error": "Requested quantity exceeds available stock"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
-        cart_item_serializer = CartItemSerializer(
-            cart_item_data, context={"request": request}
-        )
-        cart_item = cart_item_serializer.data
+            image = (
+                product.item_image if variant is None else variant.item_image
+            )
 
-        cart = Cart(request)
-        cart.add_product(cart_item)
+            if selected_option:
+                selected_label = selected_option.get("option_name")
+                selected_option_value = selected_option.get("value_name")
+                selected_option = (
+                    selected_label + " - " + selected_option_value
+                )
 
-        unique_cart_id = str(uuid.uuid4())
-        response = JsonResponse({"cart_id": unique_cart_id}, status=201)
+            if variant:
+                variant = product.variant_label + " - " + variant.name
 
-        response.set_cookie(
-            "sessionid",
-            request.session.session_key,
-            httponly=True,
-            samesite="Lax",
-        )
-        return response
+            cart_item_data = {
+                "id": product_id,
+                "name": product.name,
+                "slug": product.slug,
+                "price": str(product.current_price),
+                "variant": variant,
+                "selected_option": selected_option,
+                "quantity": quantity,
+                "available_quantity": available_quantity,
+                "image": image,
+                "url": product.get_absolute_url(),
+            }
+
+            cart_item_serializer = CartItemSerializer(
+                cart_item_data, context={"request": request}
+            )
+            cart_item = cart_item_serializer.data
+
+            cart = Cart(request)
+            cart.add_product(cart_item)
+
+            unique_cart_id = str(uuid.uuid4())
+            response = JsonResponse({"cart_id": unique_cart_id}, status=201)
+
+            response.set_cookie(
+                "sessionid",
+                request.session.session_key,
+                httponly=True,
+                samesite="Lax",
+            )
+            return response
+        return JsonResponse(serializer.errors, status=400)
 
 
 class CartUpdateView(GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = CartUpdateSerializer
 
     def post(self, request, *args, **kwargs):
-        body = request.data
-
-        cart_id = body.get("cart_id")
-        product_id = body.get("product_id")
-        quantity = int(body.get("quantity", 1))
-        variant_id = body.get("variant_id")
-
-        cart = Cart(request)
-
-        if not cart:
-            return JsonResponse({"error": "Cart ID not provided"}, status=400)
-
-        product = get_object_or_404(Product, id=product_id)
-        variant = None
-        if variant_id:
-            variant = get_object_or_404(Variant, id=variant_id)
-            available_quantity = variant.qty
-        else:
-            available_quantity = product.qty
-        existing_quantity = cart.get_product_quantity(product_id, variant)
-
-        if quantity + existing_quantity > available_quantity:
-            return Response(
-                {"error": "Brak wystarczającej ilości towaru na magazynie"},
-                status=status.HTTP_400_BAD_REQUEST,
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            cart_id = serializer.validated_data.get("cart_id")
+            product_id = serializer.validated_data.get("product_id")
+            quantity = serializer.validated_data.get("quantity")
+            variant_id = serializer.validated_data.get("variant_id")
+            selected_option = serializer.validated_data.get(
+                "selected_option", None
             )
 
-        image = product.item_image if variant is None else variant.item_image
+            cart = Cart(request)
 
-        cart_item_data = {
-            "id": product_id,
-            "name": product.name,
-            "slug": product.slug,
-            "price": str(product.current_price),
-            "variant": variant.name if variant else None,
-            "quantity": quantity,
-            "available_quantity": available_quantity,
-            "image": image,
-            "url": product.get_absolute_url(),
-        }
+            if not cart:
+                return JsonResponse(
+                    {"error": "Cart ID not provided"}, status=400
+                )
 
-        cart_item_serializer = CartItemSerializer(
-            cart_item_data, context={"request": request}
-        )
-        cart_item = cart_item_serializer.data
+            product = get_object_or_404(Product, id=product_id)
+            variant = None
+            if variant_id:
+                variant = get_object_or_404(Variant, id=variant_id)
+                available_quantity = variant.qty
+            else:
+                available_quantity = product.qty
+            existing_quantity = cart.get_product_quantity(product_id, variant)
 
-        cart.add_product(cart_item)
-        return JsonResponse({"cart_id": cart_id}, status=200)
+            if quantity + existing_quantity > available_quantity:
+                return Response(
+                    {
+                        "error": "Brak wystarczającej ilości towaru na magazynie"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            image = (
+                product.item_image if variant is None else variant.item_image
+            )
+
+            if selected_option:
+                selected_label = selected_option.get("option_name")
+                selected_option_value = selected_option.get("value_name")
+                selected_option = (
+                    selected_label + " - " + selected_option_value
+                )
+
+            if variant:
+                variant = product.variant_label + " - " + variant.name
+
+            cart_item_data = {
+                "id": product_id,
+                "name": product.name,
+                "slug": product.slug,
+                "price": str(product.current_price),
+                "variant": variant,
+                "selected_option": selected_option,
+                "quantity": quantity,
+                "available_quantity": available_quantity,
+                "image": image,
+                "url": product.get_absolute_url(),
+            }
+
+            cart_item_serializer = CartItemSerializer(
+                cart_item_data, context={"request": request}
+            )
+            cart_item = cart_item_serializer.data
+
+            cart.add_product(cart_item)
+            return JsonResponse({"cart_id": cart_id}, status=200)
+        return JsonResponse(serializer.errors, status=400)
 
 
 class GetCartItemsView(GenericAPIView):
@@ -160,29 +202,33 @@ class CartDeleteView(GenericAPIView):
 
 class UpdateCartItemQtyView(GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = UpdateCartItemQtySerializer
 
     def post(self, request, *args, **kwargs):
-        body = request.data
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            item_id = serializer.validated_data.get("item_id")
+            quantity = serializer.validated_data.get("quantity")
 
-        cart = Cart(request)
-        item_id = body.get("item_id")
-        qty = body.get("quantity")
-
-        cart.update_item_qty(item_id, qty)
-        return JsonResponse({"message": "Item qty updated"}, status=200)
+            cart = Cart(request)
+            cart.update_item_qty(item_id, quantity)
+            return JsonResponse({"message": "Item qty updated"}, status=200)
+        return JsonResponse(serializer.errors, status=400)
 
 
 class RemoveItemView(GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = RemoveItemSerializer
 
     def post(self, request, *args, **kwargs):
-        body = request.data
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            item_id = serializer.validated_data.get("item_id")
 
-        cart = Cart(request)
-        item_id = body.get("item_id")
-
-        cart.remove_item(item_id)
-        return JsonResponse({"message": "Item removed"}, status=200)
+            cart = Cart(request)
+            cart.remove_item(item_id)
+            return JsonResponse({"message": "Item removed"}, status=200)
+        return JsonResponse(serializer.errors, status=400)
 
 
 create_cart = CartCreateView.as_view()
