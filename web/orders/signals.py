67b5068850b720c2bccb8df72802315a4bbd4ce0
Michał Pielak <michal.pielak81@gmail.com>
2024-09-02 20:05:24 +0200

    quick fix

--- web/orders/signals.py
+++ web/orders/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from web.models.orders import Invoice, Order
 from web.utils import generate_invoice_for_order
 
-STATUS_TO_MAKE_INVOICE = [8, 9, 12, 13]
+STATUS_TO_MAKE_INVOICE = [3, 5, 8, 9, 12, 13]
 
 
 @receiver(post_save, sender=Order)
