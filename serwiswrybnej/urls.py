5cbb90c573fc5fe52298557ae29517a341c1d0a8
Michał Pielak <michal.pielak81@gmail.com>
2024-10-06 13:12:58 +0200

    Dodanie mailgun do aplikacji

--- serwiswrybnej/urls.py
+++ serwiswrybnej/urls.py
@@ -30,6 +30,7 @@ def trigger_error(request):
 
 urlpatterns = [
     path('sentry-debug/', trigger_error),
+    path("anymail/", include("anymail.urls")),
     path("admin/", admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
