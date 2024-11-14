ccd52f692cb2b10ae0cb969b421767b93257e935
Michał Pielak <michal.pielak81@gmail.com>
2024-11-14 21:07:18 +0100

    command v2

--- web/management/commands/sync_media_to_drive.py
+++ web/management/commands/sync_media_to_drive.py
@@ -8,7 +8,7 @@ from googleapiclient.http import MediaFileUpload
 from googleapiclient.errors import HttpError
 
 class Command(BaseCommand):
-    help = 'Synchronizuje katalog media z Google Drive, wysyłając tylko nowe lub zmienione pliki bez dublowania i usuwania'
+    help = 'Synchronizuje katalog media z Google Drive, dodając lub aktualizując pliki na podstawie nazwy i rozmiaru'
 
     def handle(self, *args, **kwargs):
         folder_path = settings.MEDIA_ROOT
@@ -24,12 +24,14 @@ class Command(BaseCommand):
             )
             service = build('drive', 'v3', credentials=credentials)
 
-            # Pobierz ID głównego folderu 'media' na Google Drive, tworząc go, jeśli nie istnieje
-            self.stdout.write("Sprawdzanie folderu 'media' na Google Drive...")
+            self.stdout.write("Pobieranie struktury folderu 'media' na Google Drive...")
             drive_media_folder_id = self.get_or_create_drive_folder(service, 'media', settings.GOOGLE_DRIVE_FOLDER_ID)
 
-            self.stdout.write("Rozpoczynanie synchronizacji lokalnego katalogu 'media' z Google Drive...")
-            self.sync_folder(service, folder_path, drive_media_folder_id)
+            # Rekurencyjne mapowanie struktury plików i folderów na Google Drive
+            drive_files_map = self.map_drive_structure(service, drive_media_folder_id)
+
+            self.stdout.write("Synchronizacja lokalnego katalogu 'media' z Google Drive...")
+            self.sync_folder(service, folder_path, drive_media_folder_id, drive_files_map)
 
             self.stdout.write(self.style.SUCCESS("Katalog 'media' został zsynchronizowany z Google Drive"))
 
@@ -55,45 +57,77 @@ class Command(BaseCommand):
             folder = service.files().create(body=file_metadata, fields='id').execute()
             return folder['id']
 
-    def sync_folder(self, service, local_folder_path, parent_id):
-        """Synchronizuje lokalny folder z Google Drive bez dublowania plików."""
+    def map_drive_structure(self, service, folder_id):
+        """Rekurencyjnie mapuje strukturę plików i folderów na Google Drive."""
+        drive_files = {}
+
+        def map_folder(drive_service, current_folder_id):
+            query = f"'{current_folder_id}' in parents"
+            results = drive_service.files().list(q=query, spaces='drive', fields='files(id, name, size, mimeType)').execute()
+            folder_contents = {}
+
+            for file in results.get('files', []):
+                if file['mimeType'] == 'application/vnd.google-apps.folder':
+                    folder_contents[file['name']] = {
+                        'id': file['id'],
+                        'type': 'folder',
+                        'children': map_folder(drive_service, file['id'])
+                    }
+                else:
+                    folder_contents[file['name']] = {
+                        'id': file['id'],
+                        'type': 'file',
+                        'size': int(file.get('size', 0))
+                    }
+            return folder_contents
+
+        drive_files = map_folder(service, folder_id)
+        return drive_files
+
+    def sync_folder(self, service, local_folder_path, parent_id, drive_files_map):
+        """Rekursywnie synchronizuje lokalny folder z Google Drive."""
         for root, dirs, files in os.walk(local_folder_path):
             relative_path = os.path.relpath(root, local_folder_path)
             folder_id = parent_id
+            current_map = drive_files_map
 
+            # Tworzymy podfoldery na Google Drive na podstawie struktury lokalnej
             if relative_path != ".":
                 for folder_name in relative_path.split(os.sep):
-                    folder_id = self.get_or_create_drive_folder(service, folder_name, folder_id)
+                    if folder_name in current_map and current_map[folder_name]['type'] == 'folder':
+                        folder_id = current_map[folder_name]['id']
+                        current_map = current_map[folder_name]['children']
+                    else:
+                        folder_id = self.get_or_create_drive_folder(service, folder_name, folder_id)
+                        current_map[folder_name] = {
+                            'id': folder_id,
+                            'type': 'folder',
+                            'children': {}
+                        }
+                        current_map = current_map[folder_name]['children']
 
-            # Pobranie listy plików z obecnego folderu na Google Drive
-            drive_files = self.get_drive_files_in_folder(service, folder_id)
-
+            # Przesyłanie plików
             for file_name in files:
                 file_path = os.path.join(root, file_name)
                 file_size = os.path.getsize(file_path)
 
-                # Sprawdzenie, czy plik już istnieje i czy ma ten sam rozmiar
-                if file_name in drive_files and drive_files[file_name]['size'] == file_size:
-                    self.stdout.write(f"Plik '{file_name}' jest aktualny na Google Drive, pomijanie.")
-                else:
-                    # Jeśli plik jest nowy lub zmieniony, przesyłamy go
-                    if file_name in drive_files:
-                        self.update_file(service, file_path, drive_files[file_name]['id'])
+                # Sprawdzenie, czy plik już istnieje w bieżącym podfolderze i czy ma ten sam rozmiar
+                if file_name in current_map and current_map[file_name]['type'] == 'file':
+                    drive_file = current_map[file_name]
+                    if file_size == drive_file['size']:
+                        self.stdout.write(f"Plik '{file_name}' jest już aktualny na Google Drive, pomijanie.")
+                        continue
                     else:
-                        self.upload_file(service, file_path, folder_id)
-
-    def get_drive_files_in_folder(self, service, folder_id):
-        """Pobiera listę plików w folderze Google Drive, wraz z ich ID i rozmiarem."""
-        query = f"'{folder_id}' in parents"
-        results = service.files().list(q=query, spaces='drive', fields='files(id, name, size)').execute()
-        
-        drive_files = {}
-        for file in results.get('files', []):
-            drive_files[file['name']] = {
-                'id': file['id'],
-                'size': int(file.get('size', 0))
-            }
-        return drive_files
+                        # Nadpisanie zmienionego pliku
+                        self.update_file(service, file_path, drive_file['id'])
+                else:
+                    # Przesyłanie nowego pliku
+                    self.upload_file(service, file_path, folder_id)
+                    current_map[file_name] = {
+                        'id': None,  # Do ustalenia, ale na razie pomijamy to po stronie Google Drive
+                        'type': 'file',
+                        'size': file_size
+                    }
 
     def upload_file(self, service, file_path, parent_id):
         """Przesyła nowy plik na Google Drive."""
