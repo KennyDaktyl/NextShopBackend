baec248072639a8f9e093b75f0da8e83ef04e6b9
Michał Pielak <michal.pielak81@gmail.com>
2024-11-14 16:28:43 +0100

    command v2

--- web/management/commands/sync_media_to_drive.py
+++ web/management/commands/sync_media_to_drive.py
@@ -4,123 +4,105 @@ from django.core.management.base import BaseCommand
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import HttpError
 
 class Command(BaseCommand):
-    help = 'Optimized synchronization of the local media directory with Google Drive folder, preserving folder structure'
-
-    def add_arguments(self, parser):
-        parser.add_argument('folder_path', type=str, help='Path to the local media folder to be synchronized')
+    help = 'Replaces entire media directory on Google Drive with local version using MEDIA_ROOT'
 
     def handle(self, *args, **kwargs):
-        folder_path = kwargs['folder_path']
+        # Pobierz lokalizację katalogu 'media' z MEDIA_ROOT
+        folder_path = settings.MEDIA_ROOT
         
         if not os.path.isdir(folder_path):
             self.stdout.write(self.style.ERROR(f'Folder {folder_path} does not exist'))
             return
 
         try:
-            # Uwierzytelnienie konta usługi
             credentials = Credentials.from_service_account_file(
                 settings.GOOGLE_SERVICE_ACCOUNT_JSON_PATH,
                 scopes=["https://www.googleapis.com/auth/drive"]
             )
             service = build('drive', 'v3', credentials=credentials)
 
-            # Tworzenie rekurencyjnego odwzorowania struktury folderu Google Drive
-            def map_drive_structure(drive_folder_id):
-                results = service.files().list(
-                    q=f"'{drive_folder_id}' in parents",
-                    spaces='drive',
-                    fields='files(id, name, mimeType, modifiedTime)').execute()
-                
-                drive_structure = {}
-                for file in results.get('files', []):
-                    if file['mimeType'] == 'application/vnd.google-apps.folder':
-                        drive_structure[file['name']] = {
-                            'id': file['id'],
-                            'type': 'folder',
-                            'children': map_drive_structure(file['id'])
-                        }
-                    else:
-                        drive_structure[file['name']] = {
-                            'id': file['id'],
-                            'type': 'file',
-                            'modifiedTime': file['modifiedTime']
-                        }
-                return drive_structure
+            # Usuń istniejący folder 'media' na Google Drive, jeśli istnieje
+            self.stdout.write("Deleting existing 'media' folder on Google Drive...")
+            self.delete_drive_folder(service, 'media', settings.GOOGLE_DRIVE_FOLDER_ID)
 
-            # Pobieramy strukturę folderu "media" na Google Drive
-            drive_media_folder_id = self.get_or_create_drive_folder(service, 'media', settings.GOOGLE_DRIVE_FOLDER_ID)
-            drive_files = map_drive_structure(drive_media_folder_id)
+            # Twórz nowy folder 'media' i przesyłaj całą strukturę
+            self.stdout.write("Creating new 'media' folder on Google Drive...")
+            drive_media_folder_id = self.create_drive_folder(service, 'media', settings.GOOGLE_DRIVE_FOLDER_ID)
+            
+            self.stdout.write("Uploading local media folder to Google Drive...")
+            self.upload_folder(service, folder_path, drive_media_folder_id)
 
-            # Mapa plików lokalnych do porównania
-            local_files = {}
-            for root, _, files in os.walk(folder_path):
-                relative_path = os.path.relpath(root, folder_path)
-                local_files[relative_path] = {
-                    f: os.path.getmtime(os.path.join(root, f)) for f in files
-                }
-
-            # Synchronizacja
-            self.sync_folders(service, drive_files, local_files, drive_media_folder_id, folder_path)
+            self.stdout.write(self.style.SUCCESS("Media folder successfully replaced on Google Drive"))
 
         except Exception as e:
-            self.stdout.write(self.style.ERROR(f'Error during synchronization: {str(e)}'))
+            self.stdout.write(self.style.ERROR(f'Error during upload: {str(e)}'))
 
-    def sync_folders(self, service, drive_files, local_files, parent_id, base_folder):
-        # Usuwanie plików z Google Drive, które zostały usunięte lokalnie
-        for drive_filename, drive_file in drive_files.items():
-            if drive_file['type'] == 'file':
-                if drive_filename not in local_files.get("", {}):
-                    service.files().delete(fileId=drive_file['id']).execute()
-                    self.stdout.write(self.style.SUCCESS(f"Deleted '{drive_filename}' from Google Drive"))
-            elif drive_file['type'] == 'folder' and drive_filename not in local_files:
-                service.files().delete(fileId=drive_file['id']).execute()
-                self.stdout.write(self.style.SUCCESS(f"Deleted folder '{drive_filename}' from Google Drive"))
-
-        # Dodawanie i aktualizowanie plików na Google Drive
-        for relative_path, files in local_files.items():
-            current_drive_folder_id = parent_id
-            if relative_path != ".":
-                for folder in relative_path.split(os.sep):
-                    current_drive_folder_id = self.get_or_create_drive_folder(service, folder, current_drive_folder_id)
-
-            for filename, local_modified_time in files.items():
-                local_file_path = os.path.join(base_folder, relative_path, filename)
-                if filename in drive_files.get(relative_path, {}):
-                    drive_file = drive_files[relative_path][filename]
-                    drive_modified_time = drive_file['modifiedTime']
-                    if local_modified_time > int(drive_modified_time.timestamp()):
-                        media = MediaFileUpload(local_file_path, resumable=True)
-                        service.files().update(fileId=drive_file['id'], media_body=media).execute()
-                        self.stdout.write(self.style.SUCCESS(f"Updated '{filename}' on Google Drive"))
+    def delete_drive_folder(self, service, folder_name, parent_id):
+        """Usuwa folder na Google Drive według nazwy, jeśli istnieje."""
+        query = f"name='{folder_name}' and mimeType='application/vnd.google-apps.folder' and '{parent_id}' in parents"
+        results = service.files().list(q=query, spaces='drive', fields='files(id)').execute()
+        
+        for file in results.get('files', []):
+            try:
+                service.files().delete(fileId=file['id']).execute()
+                self.stdout.write(self.style.SUCCESS(f"Deleted folder '{folder_name}' on Google Drive"))
+            except HttpError as error:
+                if error.resp.status == 404:
+                    self.stdout.write(self.style.WARNING(f"Folder '{folder_name}' already deleted"))
                 else:
-                    file_metadata = {
-                        'name': filename,
-                        'parents': [current_drive_folder_id]
-                    }
-                    media = MediaFileUpload(local_file_path, resumable=True)
-                    uploaded_file = service.files().create(
-                        body=file_metadata,
-                        media_body=media,
-                        fields='id'
-                    ).execute()
-                    self.stdout.write(self.style.SUCCESS(f"Uploaded new file '{filename}' to Google Drive"))
+                    raise
 
-    def get_or_create_drive_folder(self, service, folder_name, parent_id=None):
-        query = f"name='{folder_name}' and mimeType='application/vnd.google-apps.folder'"
-        if parent_id:
-            query += f" and '{parent_id}' in parents"
-        results = service.files().list(q=query, spaces='drive', fields='files(id, name)').execute()
+    def create_drive_folder(self, service, folder_name, parent_id=None):
+        """Tworzy folder na Google Drive."""
+        file_metadata = {
+            'name': folder_name,
+            'mimeType': 'application/vnd.google-apps.folder',
+            'parents': [parent_id] if parent_id else []
+        }
+        folder = service.files().create(body=file_metadata, fields='id').execute()
+        return folder['id']
+
+    def upload_folder(self, service, folder_path, parent_id):
+        """Rekursywnie przesyła lokalny folder na Google Drive."""
+        for root, dirs, files in os.walk(folder_path):
+            relative_path = os.path.relpath(root, folder_path)
+            folder_id = parent_id
+
+            if relative_path != ".":
+                # Tworzymy podfoldery na Google Drive
+                for folder_name in relative_path.split(os.sep):
+                    folder_id = self.get_or_create_subfolder(service, folder_name, folder_id)
+
+            # Przesyłanie plików w aktualnym folderze
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                self.upload_file(service, file_path, folder_id)
+
+    def get_or_create_subfolder(self, service, folder_name, parent_id):
+        """Tworzy lub pobiera istniejący podfolder na Google Drive."""
+        query = f"name='{folder_name}' and mimeType='application/vnd.google-apps.folder' and '{parent_id}' in parents"
+        results = service.files().list(q=query, spaces='drive', fields='files(id)').execute()
         folders = results.get('files', [])
-
+        
         if folders:
             return folders[0]['id']
         else:
-            file_metadata = {
-                'name': folder_name,
-                'mimeType': 'application/vnd.google-apps.folder',
-                'parents': [parent_id] if parent_id else []
-            }
-            folder = service.files().create(body=file_metadata, fields='id').execute()
-            return folder['id']
+            return self.create_drive_folder(service, folder_name, parent_id)
+
+    def upload_file(self, service, file_path, parent_id):
+        """Przesyła pojedynczy plik do określonego folderu na Google Drive."""
+        file_name = os.path.basename(file_path)
+        file_metadata = {
+            'name': file_name,
+            'parents': [parent_id]
+        }
+        media = MediaFileUpload(file_path, resumable=True)
+        service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields='id'
+        ).execute()
+        self.stdout.write(self.style.SUCCESS(f"Uploaded '{file_name}' to Google Drive"))
