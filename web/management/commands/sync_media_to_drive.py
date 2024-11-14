73f2b803afdd298bd62ed62bf3fd022b337e1092
Michał Pielak <michal.pielak81@gmail.com>
2024-11-14 17:18:50 +0100

    command v2

--- web/management/commands/sync_media_to_drive.py
+++ web/management/commands/sync_media_to_drive.py
@@ -7,42 +7,42 @@ from googleapiclient.http import MediaFileUpload
 from googleapiclient.errors import HttpError
 
 class Command(BaseCommand):
-    help = 'Optimized replacement of media directory on Google Drive with local version using MEDIA_ROOT'
+    help = 'Zastępuje katalog media na Google Drive wersją lokalną, używając MEDIA_ROOT'
 
     def handle(self, *args, **kwargs):
         # Pobierz lokalizację katalogu 'media' z MEDIA_ROOT
         folder_path = settings.MEDIA_ROOT
         
         if not os.path.isdir(folder_path):
-            self.stdout.write(self.style.ERROR(f'Folder {folder_path} does not exist'))
+            self.stdout.write(self.style.ERROR(f'Folder {folder_path} nie istnieje'))
             return
 
         try:
+            # Autoryzacja do Google Drive
             credentials = Credentials.from_service_account_file(
                 settings.GOOGLE_SERVICE_ACCOUNT_JSON_PATH,
                 scopes=["https://www.googleapis.com/auth/drive"]
             )
             service = build('drive', 'v3', credentials=credentials)
 
-            # Usuń istniejący folder 'media' na Google Drive jednym zapytaniem, jeśli istnieje
-            self.stdout.write("Deleting existing 'media' folder on Google Drive...")
+            # Usuń istniejący folder 'media' na Google Drive, jeśli istnieje
+            self.stdout.write("Usuwam istniejący folder 'media' na Google Drive...")
             media_folder_id = self.get_drive_folder_id(service, 'media', settings.GOOGLE_DRIVE_FOLDER_ID)
             if media_folder_id:
                 service.files().delete(fileId=media_folder_id).execute()
-                self.stdout.write(self.style.SUCCESS("Deleted existing 'media' folder on Google Drive"))
+                self.stdout.write(self.style.SUCCESS("Usunięto istniejący folder 'media' na Google Drive"))
 
-            # Twórz nowy folder 'media' i przesyłaj całą strukturę
-            self.stdout.write("Creating new 'media' folder on Google Drive...")
+            # Tworzenie nowego folderu 'media' i przesyłanie całości zawartości
+            self.stdout.write("Tworzenie nowego folderu 'media' na Google Drive...")
             drive_media_folder_id = self.create_drive_folder(service, 'media', settings.GOOGLE_DRIVE_FOLDER_ID)
             
-            self.stdout.write("Uploading local media folder to Google Drive...")
-            created_folders = {drive_media_folder_id: 'media'}
-            self.upload_folder(service, folder_path, drive_media_folder_id, created_folders)
+            self.stdout.write("Przesyłanie lokalnego katalogu media na Google Drive...")
+            self.upload_folder(service, folder_path, drive_media_folder_id)
 
-            self.stdout.write(self.style.SUCCESS("Media folder successfully replaced on Google Drive"))
+            self.stdout.write(self.style.SUCCESS("Folder media został pomyślnie zastąpiony na Google Drive"))
 
         except Exception as e:
-            self.stdout.write(self.style.ERROR(f'Error during upload: {str(e)}'))
+            self.stdout.write(self.style.ERROR(f'Błąd podczas przesyłania: {str(e)}'))
 
     def get_drive_folder_id(self, service, folder_name, parent_id):
         """Pobiera ID folderu na Google Drive, jeśli istnieje."""
@@ -61,34 +61,20 @@ class Command(BaseCommand):
         folder = service.files().create(body=file_metadata, fields='id').execute()
         return folder['id']
 
-    def upload_folder(self, service, folder_path, parent_id, created_folders):
-        """Rekursywnie przesyła lokalny folder na Google Drive, buforując już utworzone foldery."""
+    def upload_folder(self, service, folder_path, parent_id):
+        """Rekursywnie przesyła wszystkie pliki i podfoldery z lokalnego folderu 'media'."""
         for root, dirs, files in os.walk(folder_path):
             relative_path = os.path.relpath(root, folder_path)
             folder_id = parent_id
 
+            # Tworzymy podfoldery na Google Drive na podstawie struktury lokalnej
             if relative_path != ".":
-                # Tworzymy podfoldery na Google Drive tylko raz i buforujemy je w `created_folders`
-                for folder_name in relative_path.split(os.sep):
-                    if folder_name not in created_folders:
-                        folder_id = self.get_or_create_subfolder(service, folder_name, folder_id, created_folders)
-                    else:
-                        folder_id = created_folders[folder_name]
+                folder_id = self.create_drive_folder(service, relative_path, folder_id)
 
             # Przesyłanie plików w aktualnym folderze
             for file_name in files:
                 file_path = os.path.join(root, file_name)
                 self.upload_file(service, file_path, folder_id)
-
-    def get_or_create_subfolder(self, service, folder_name, parent_id, created_folders):
-        """Tworzy lub pobiera istniejący podfolder na Google Drive i buforuje go."""
-        folder_id = self.get_drive_folder_id(service, folder_name, parent_id)
-        
-        if not folder_id:
-            folder_id = self.create_drive_folder(service, folder_name, parent_id)
-        
-        created_folders[folder_name] = folder_id
-        return folder_id
 
     def upload_file(self, service, file_path, parent_id):
         """Przesyła pojedynczy plik do określonego folderu na Google Drive."""
@@ -97,4 +83,17 @@ class Command(BaseCommand):
             'name': file_name,
             'parents': [parent_id]
         }
-        media = MediaFileUpload
+        media = MediaFileUpload(file_path, chunksize=256*1024, resumable=True)
+        upload = service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields='id'
+        )
+
+        # Przesyłanie z potwierdzeniem ukończenia
+        response = None
+        while response is None:
+            status, response = upload.next_chunk()
+            if status:
+                self.stdout.write(f"Przesyłanie {file_name}: {int(status.progress() * 100)}% zakończone")
+        self.stdout.write(self.style.SUCCESS(f"Przesłano '{file_name}' na Google Drive"))
